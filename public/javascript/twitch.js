887351079134faeabc0c6cdfa35cc291e4a699e4
catshark <cathalsharkey@gmail.com>
2016-11-20 20:53:58 +0000

    removed comments and redundant code from twitch.js

--- public/javascript/twitch.js
+++ public/javascript/twitch.js
@@ -1,34 +1,7 @@
-var freecodecamp, esl, test;
 var streamInfo = [];
 
 
 function getTwitchStream(stream, callback) {
-    /*var request = new XMLHttpRequest();
-    var requestUrl = 'https://wind-bow.hyperdev.space/twitch-api/streams/' + stream +  '?callback=?';
-
-    request.open('GET', requestUrl, true);
-
-    request.onload = function() {
-        if (request.readyState === 4) {
-            if ((request.status >= 200 && request.status < 300) || request.status == 304) {
-                // Success!
-                var data = JSON.parse(request.responseText);
-                if (data.stream) {
-                    var wikiPages = data.query.pages;
-
-                    for (var page in wikiPages) {
-                        if (wikiPages.hasOwnProperty(page)) {
-                            wikiInfo.push({url: wikiPages[page].fullurl, title: wikiPages[page].title, extract: wikiPages[page].extract});
-                        }
-                    }
-                    streamInfo.push({url: data.stream.channel.url, displayName: data.stream.channel.display_name, status: data.stream.channel.status});
-                    callback(streamInfo);
-                }
-            }
-        }
-    };
-
-    request.send();*/
     var requestUrl = 'https://wind-bow.hyperdev.space/twitch-api/streams/' + stream +  '?callback=?';
 
     $.getJSON(requestUrl, function(data) {
